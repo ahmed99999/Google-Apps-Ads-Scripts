db9975e7fa9b731fd379420c9129f212f22c692e
Ahmed Bakhabou <a.bakhabou@pa.ag>
2020-01-21 18:22:49 +0100

    hr_bot: new bot using electron

---
+++ hr_bot/V2/index.js
@@ -0,0 +1,18 @@
+const electron = require('electron');
+const url = require('url');
+const path = require('path');
+const { app, BrowserWindow } = electron;
+let mainWindow;
+
+// listen for the app to be ready
+
+app.on( 'ready', function (){
+     mainWindow = new BrowserWindow({
+        width: 1000
+     });
+     mainWindow.loadURL( url.format({
+         pathname: path.join( __dirname, 'mainWindow.html'),
+         protocol: 'file:',
+         slashes: true
+     }));
+});
